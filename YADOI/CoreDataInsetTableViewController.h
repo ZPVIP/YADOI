3ead88265bd4e174dfaacf8c2f74f228ae0a0200
HaiLee <comcuter@gmail.com>
2012-10-30 15:18:07 +0800

    重构一下代码

---
+++ YADOI/CoreDataInsetTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataInsetTableViewController.h
+//  YADOI
+//
+//  Created by HaiLee on 12-10-30.
+//  Copyright (c) 2012年 HaiLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataInsetTableViewController : NSObject
+
+@end
---
+++ YADOI/CoreDataInsetTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataInsetTableViewController.m
+//  YADOI
+//
+//  Created by HaiLee on 12-10-30.
+//  Copyright (c) 2012年 HaiLee. All rights reserved.
+//
+
+#import "CoreDataInsetTableViewController.h"
+
+@implementation CoreDataInsetTableViewController
+
+@end
